2a448d1b1f27dd5226960062eecd91d2203d3e40
Szymon <szymon.typko@gmail.com>
2026-09-16 17:08:47 +0200

    Dodano wstępne przesyłanie zdjęcia

--- solutions/01-MQTT/mqtt_handler.cpp
+++ solutions/01-MQTT/mqtt_handler.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include <mbedtls/base64.h>
 
 #include "config.h"
 #include "mqtt_handler.h"
+#include "camera_handler.h"
 
-namespace {
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
 
@@ -25,7 +26,78 @@ void reconnect() {
 		}
 	}
 }
+
+
+void takeAndSendPhoto() {
+  Serial.println("[KAMERA] Robię zdjęcie...");
+  camera_fb_t* fb = esp_camera_fb_get();
+  if (fb) { esp_camera_fb_return(fb); }
+  fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("[KAMERA] Błąd pobierania ramki z kamery");
+    return;
+  }
+  Serial.printf("[KAMERA] Zdjęcie zrobione! Rozmiar JPEG: %u B\n", fb->len);
+
+  String base64Prefix = "data:image/jpeg;base64,";
+  size_t prefixLen = base64Prefix.length();
+  size_t base64DataLen = 0;
+  mbedtls_base64_encode(NULL, 0, &base64DataLen, fb->buf, fb->len);
+  size_t totalPayloadLen = prefixLen + base64DataLen;
+  size_t maxPayloadLen = 65535 - 5 - 2 - strlen(MQTTConfig::topic_foto);
+  Serial.printf("[MQTT] Base64: %u B, limit payloadu: %u B\n", totalPayloadLen, maxPayloadLen);
+
+  char* payloadBuffer = (char*)ps_malloc(totalPayloadLen + 1);
+  if (!payloadBuffer) {
+    payloadBuffer = (char*)malloc(totalPayloadLen + 1);
+  }
+
+  if (payloadBuffer) {
+    memcpy(payloadBuffer, base64Prefix.c_str(), prefixLen);
+    size_t written = 0;
+    mbedtls_base64_encode((unsigned char*)(payloadBuffer + prefixLen), base64DataLen + 1, &written, fb->buf, fb->len);
+    payloadBuffer[totalPayloadLen] = '\0';
+    Serial.printf("[MQTT] Wysyłam Base64 (Łącznie: %u B)...\n", totalPayloadLen);
+
+    bool success = client.beginPublish(MQTTConfig::topic_foto, totalPayloadLen, true);
+    if (success) {
+      const uint8_t* payload = (const uint8_t*)payloadBuffer;
+      size_t bytesSent = 0;
+      constexpr size_t chunkSize = 1024;
+      while (bytesSent < totalPayloadLen) {
+        size_t bytesToSend = min(chunkSize, totalPayloadLen - bytesSent);
+        size_t writtenChunk = client.write(payload + bytesSent, bytesToSend);
+        if (writtenChunk != bytesToSend) {
+          success = false;
+          break;
+        }
+        bytesSent += writtenChunk;
+      }
+      if (success) {
+        success = client.endPublish();
+      }
+    }
+    if (success) {
+      Serial.println("[MQTT] Zdjęcie wysłane pomyślnie!");
+      unsigned long totalSec = millis() / 1000;
+      unsigned long min = (totalSec / 60) % 60;
+      unsigned long hrs = totalSec / 3600;
+      unsigned long sec = totalSec % 60;
+      char timeStr[64];
+      snprintf(timeStr, sizeof(timeStr), "Zdjęcie z %02luh %02lum %02lus (Uptime)", hrs, min, sec);
+      client.publish(MQTTConfig::topic_timestamp, timeStr);
+      Serial.printf("[MQTT] Timestamp wysłany: %s\n", timeStr);
+    } else {
+      Serial.printf("[MQTT] Publikacja nieudana. Stan klienta: %d, payload: %u/%u B\n",
+                    client.state(), totalPayloadLen, maxPayloadLen);
+    }
+    free(payloadBuffer);
+  } else {
+    Serial.println("[BŁĄD] Brak pamięci na bufor Base64");
+  }
+  esp_camera_fb_return(fb);
 }
+
 void callback(char* topic, byte* payload, unsigned int length) {
   String message = "";
   for (int i = 0; i < length; i++) {
@@ -34,7 +106,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.printf("[MQTT] Odebrano komendę na [%s]: %s\n", topic, message.c_str());
   String top = String(topic);
   if (top.endsWith("/capture")) {
-    // zrob zdjecie
+    takeAndSendPhoto();
   } else if (top.endsWith("/brightness")) {
     // ustaw jasność
   } else if (top.endsWith("/contrast")) {
@@ -58,7 +130,7 @@ void mqttSetup() {
 	client.setServer(MQTTConfig::mqtt_server, MQTTConfig::mqtt_port);
 	client.setCallback(callback);
 
-	// pewnie wiekszy bufor dla przesylania zdjec
+	Serial.println(client.setBufferSize(65535) ? "udalo sie ustawic bufor 65K" : "nie udalo sie ustawic bufora 65K");// 65535
 }
 
 void mqttLoop() {
--- solutions/01-MQTT/mqtt_handler.h
+++ solutions/01-MQTT/mqtt_handler.h
@@ -1,4 +1,6 @@
 #pragma once
 
+#include <PubSubClient.h>
+void takeAndSendPhoto();
 void mqttSetup();
 void mqttLoop();
